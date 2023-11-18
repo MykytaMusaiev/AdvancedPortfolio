52618f57b693f5ed3a8b44edc3fd92ce4524c301
Nikita Musaiev <n.musaiev@gmail.com>
2023-11-18 17:59:44 +0200

    Skill control component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,42 @@
 import React from 'react'
+import { motion } from "framer-motion"
+
+const Skill = ({ name, x, y }) => {
+    return (
+        <motion.div
+            className="flex items-center justify-center rounded-full font-semibold
+                     bg-dark text-light py-3 px-6 shadow-dark cursor-pointer absolute"
+            whileHover={{ scale: 1.05 }}
+            initial={{x:0, y:0}}
+            animate={{x:x, y:y}}
+            transition={{ duration: 1.5}}
+        >
+            {name}
+        </motion.div>
+    )
+}
 
 const Skills = () => {
-  return (
-    <div>Skills</div>
-  )
+    return (
+        <>
+            <h2 className="font-bold text-8xl w-full text-center">Skills</h2>
+            <div className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight"> 
+                <Skill name="Web" x="0vw" y="0vw"/>                         
+                <Skill name="Web Design" x="0vw" y="-22vw"/>      
+                <Skill name="HTML5" x="-18vw" y="-15vw"/>
+                <Skill name="CSS3" x="0vw" y="-12vw"/>
+                <Skill name="Javascript ES9" x="15vw" y="-14vw"/>
+
+                <Skill name="ReactJS" x="-25vw" y="-2vw"/>
+                <Skill name="Tailwind CSS" x="23vw" y="1vw"/>
+
+                <Skill name="MaterialUI" x="-11vw" y="7vw"/>
+                <Skill name="Styled Components" x="10vw" y="11vw"/>
+
+                <Skill name="Github" x="-4vw" y="17vw"/>
+            </div>
+        </>
+    )
 }
 
 export default Skills
